a84aadca34edd7128861600c559fdcf02563cb09
Carlos Tapia <ctapiadev@gmail.com>
2025-06-30 17:00:27 -0400

    clase 3 js

---
+++ M2-Clase3-if-else/FichaPersonaje/app.js
@@ -0,0 +1,60 @@
+console.log("-------------------------")
+console.log("--- Actividad Clase 1 ---")
+console.log("-------------------------")
+
+const characterName = "Vegeta";
+const characterRace = "Saiyan";
+let currentLevel = 160;
+let healthPoints = 1000;
+let isAlive = true;
+
+console.log("--- Ficha de Personaje ---")
+console.log("Nombre:", characterName);
+console.log("Raza:", characterRace);
+console.log("Nivel:", currentLevel);
+console.log("PV:", healthPoints);
+console.log("¿Está vivo?:", isAlive);
+
+console.log("");
+console.log("-------------------------")
+console.log("--- Actividad Clase 2 ---")
+console.log("-------------------------")
+
+// --- Cálculos de Atributos ---
+const attackPower = currentLevel * 12;
+let defensePoints = healthPoints / 4;
+
+const characterBio = `${characterName} (${characterRace}): Nivel ${currentLevel}`
+
+// --- Verificaciones Lógicas ---
+const isPowerful = attackPower > 100;
+const canEnterDungeon = (currentLevel >= 100) && (isAlive === true);
+
+console.log(characterBio);
+console.log(`Poder de Ataque: ${attackPower}`);
+console.log(`Puntos de Defensa: ${defensePoints}`);
+console.log(`¿Es poderoso?: ${isPowerful}`);
+console.log(`¿Puede entrar en una mazmorra?: ${canEnterDungeon}`);
+
+console.log("");
+console.log("-------------------------")
+console.log("--- Actividad Clase 3 ---")
+console.log("-------------------------")
+// --- Decisión de Batalla ---
+if (healthPoints > 70) {
+    console.log(`¡${characterName} está en plena forma!`);
+} else if (healthPoints > 30) {
+    console.log(`¡${characterName} está herido, cuidado!`);
+} else {
+    console.log(`¡PELIGRO! ¡${characterName} necesita ayuda urgente!`);
+}
+
+// --- Cofre del Tesoro ---
+let hasGoldenKey = true;
+let enemyGuarding = false;
+
+if (hasGoldenKey && !enemyGuarding) {
+    console.log("¡El cofre se abre y revela un tesoro!");
+} else {
+    console.log("El camino hacia el cofre está bloqueado...");
+}
---
+++ M2-Clase3-if-else/app.js
@@ -0,0 +1,60 @@
+
+// --- If/else ---
+
+let tengoHambre = true;
+
+if (tengoHambre === true) {
+    console.log("Voy a comer una pizza 🍕");
+} else {
+    console.log("No tengo hambre.");
+}
+
+// --- PRACTICA ---
+let userAge = 18;
+
+if (userAge >= 18) {
+    console.log("Acceso permitido al contenido.");
+} else {
+    console.log("Acceso denegado.");
+}
+
+// --- if / else if / else ---
+let nota = 4.0;
+
+if (nota === 7.0) {
+    console.log("¡Excelente!");
+} else if (nota >= 4.0 && nota < 7.0) { // true && true = true
+    console.log("Buen trabajo");
+} else if (nota >= 1.0 && nota < 4.0) {
+    console.log("Necesitas estudiar más");
+} else {
+    console.log("Se debe ingresar un dato valido");
+}
+
+
+// --- && , || y ! (Y, O y NOT) ---
+
+let edad = 25;
+let esHombre = true;
+
+console.log(edad >= 20 && edad <= 30);
+console.log(edad >= 18 && esHombre === true); // true && true = true
+console.log(edad >= 18 && esHombre === false); // true && false = false
+
+console.log(edad >= 18 || esHombre === false);  // true || false = true
+console.log(edad < 18 || esHombre === false); // false || false = false
+
+console.log(!esHombre);
+
+// --- Falsy ---
+
+let dato = 5 + 5;
+
+if (dato) {
+    console.log("El dato es un Truthy");
+} else {
+    console.log("El dato es un Falsy");
+}
+
+let prueba = null;
+console.log(typeof prueba);
