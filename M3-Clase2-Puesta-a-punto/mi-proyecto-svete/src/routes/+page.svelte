403b5bd416a9493ec053441e19cc34f21cc42e75
Carlos Tapia <ctapiadev@gmail.com>
2025-08-01 18:02:59 -0400

    m3-clase2

---
+++ M3-Clase2-Puesta-a-punto/mi-proyecto-svete/src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    let count = 0;
+
+    function increment() {
+        count += 1;
+    }
+
+    function decrement() {
+        count -= 1;
+    }
+</script>
+
+<main>
+    <h1>Contador</h1>
+    <p>El valor actual es: {count}</p>
+    <button on:click={increment}>
+        Aumentar
+    </button>
+    <button on:click={decrement}>
+        Disminuir
+    </button>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 32px;
+    }
+
+    h1 {
+        color: chocolate;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+</style>
