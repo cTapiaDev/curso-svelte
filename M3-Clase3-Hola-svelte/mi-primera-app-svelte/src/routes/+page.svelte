56a26a38091d130ecba43d6ed295f43090ac588c
Carlos Tapia <ctapiadev@gmail.com>
2025-08-06 19:39:08 -0400

    m3 svelte

---
+++ M3-Clase3-Hola-svelte/mi-primera-app-svelte/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	let userName = "Aragorn";
+	let level = 5;
+
+	const user = {
+		email: "aragorn@mordor.so"
+	}
+
+	let contador = 0;
+
+	function aumentar() {
+		contador++;
+	}
+
+	function disminuir() {
+		contador--;
+	}
+
+</script>
+
+<h1>Bienvenido, {userName}</h1>
+
+<p>Tu nivel es: {level * 10}</p>
+<p>Email: {user.email}</p>
+
+<h2>{contador}</h2>
+
+<button on:click={aumentar}>Aumentar</button>
+<button on:click={disminuir}>Disminuir</button>
+
+<style>
+
+</style>
---
+++ M3-Clase3-Hola-svelte/mi-primera-app-svelte/src/routes/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Hola desde la ruta About</h1>
