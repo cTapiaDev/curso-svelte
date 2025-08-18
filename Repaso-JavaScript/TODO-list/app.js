56df966cfd99b0ff4c4d53bb1bc44986445aa510
Carlos Tapia <ctapiadev@gmail.com>
2025-08-18 14:24:47 -0400

    todo list

---
+++ Repaso-JavaScript/TODO-list/app.js
@@ -0,0 +1,77 @@
+
+// --- SELECCIÓN DE ELEMENTOS DEL DOM ---
+const formularioTareas = document.querySelector('#formulario-nueva-tarea');
+const inputTarea = document.querySelector('#input-nueva-tarea');
+const listaTareas = document.querySelector('#lista-tareas');
+const contadorTareas = document.querySelector('#contador-tareas');
+
+// --- MANEJO DE ESTADO ---
+let tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+
+// --- FUNCIONES EXTRAS ---
+const guardarTareas = () => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+};
+
+const actualizarContador = () => {
+    const tareasPendientes = tareas.filter(tarea => !tarea.completada).length;
+    contadorTareas.textContent = `Tienes ${tareasPendientes} tarea(s) pendiente(s).`;
+}
+
+// --- FUNCIÓN PRINCIPAL ---
+const renderizarTareas = () => {
+    listaTareas.innerHTML = '';
+
+    if (tareas.length === 0) {
+        listaTareas.innerHTML = '<li class="sin-tareas"><p>¡Felicidades, no hay tareas pendientes!</p></li>';
+        actualizarContador();
+        return;s
+    }
+
+    tareas.forEach(tarea => {
+        const li = document.createElement('li');
+        li.setAttribute('data-id', tarea.id);
+
+        if (tarea.completada) {
+            li.classList.add('completada');
+        }
+
+        li.innerHTML = `
+            <span>${tarea.text}</span>
+            <button class="boton-borrar">X</button>
+        `
+
+        listaTareas.appendChild(li);
+    });
+
+    actualizarContador();
+}
+
+// --- MANEJO DE EVENTOS ---
+const agregarTarea = (evento) => {
+    evento.preventDefault();
+    const textoTarea = inputTarea.value.trim();
+
+    if (textoTarea === '') {
+        alert('Por favor, escribe una tarea.');
+        return;
+    }
+
+    const nuevaTarea = {
+        id: Date.now(),
+        text: textoTarea,
+        completada: false,
+    }
+
+    tareas.unshift(nuevaTarea); // Esto añade la tarea al principio.
+
+    guardarTareas();
+    renderizarTareas();
+
+    inputTarea.value = '';
+}
+
+// INICIALIZACIÓN //
+
+document.addEventListener('DOMContentLoaded', renderizarTareas);
+formularioTareas.addEventListener('submit', agregarTarea);
